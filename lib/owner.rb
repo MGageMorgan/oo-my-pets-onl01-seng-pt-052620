e30de363a4bf39384de354721c9a514f66d19484
Gage Morgan <gage@gages.blog>
2020-06-29 00:31:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,11 @@
 class Owner
   attr_reader :name, :species
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def species
@@ -12,4 +15,7 @@ class Owner
   def say_species
     "I am a human."
   end
+
+  self.all
+  
 end
